1396ce1a8af149bce1938c641962ca81d4862234
benji6 <benhall22@hotmail.co.uk>
2015-05-26 16:42:48 +0100

    starting work on control pad

--- src/cljs/elemental/view.cljs
+++ src/cljs/elemental/view.cljs
@@ -1,8 +1,20 @@
 (ns elemental.view
     (:require [reagent.core :as reagent :refer [atom]]))
 
+(defn handle-touch-pad-input [e]
+  (let [bounding-rect (.getBoundingClientRect (.-target e))
+        x-offset (- (.-clientX e) (.-left bounding-rect))
+        y-offset (- (.-clientY e) (.-top bounding-rect))]
+    (println x-offset y-offset)))
+
+(defn handle-touch-pad-input-stop [e]
+    (println "leavin' pad"))
+
 (defn view []
-  [:div [:h1 "Elemental"]])
+  [:div.center [:h1 "Elemental"]
+        [:h3 "Instructions"]
+        [:canvas {:on-mouse-move handle-touch-pad-input
+                  :on-mouse-out handle-touch-pad-input-stop}]])
 
 (defn mount-root []
   (reagent/render [view] (.getElementById js/document "app")))
